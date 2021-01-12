235f4ab2cca4d2bfeddc4c13619759cb05b53327
Raphael Suter <raphael.werner.suter@gmail.com>
2021-01-12 09:54:47 +0100

    Aufgabe 1 & 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Container, ListGroup } from "react-bootstrap";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h1>Hello World</h1>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const App = () => {
+  const [deckID, setDeckID] = useState("");
+  const [cards, setCards] = useState([]);
+
+  const newDeck = () => {
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+      .then((response) => response.json())
+      .then(({ deck_id }) => {
+        setDeckID(deck_id);
+      });
+  };
+
+  const newCard = () => {
+    fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
+      .then((response) => response.json())
+      .then(({ cards: newCards }) => {
+        setCards([...newCards, ...cards]);
+      });
+  };
+
+  return (
+    <Container className="my-5">
+      <div className="d-flex align-items-center">
+        <Button onClick={newDeck}>New Deck</Button>
+        <p className="m-0 ml-3">{deckID}</p>
+      </div>
+      <Button className="mt-4" onClick={newCard}>
+        New Card
+      </Button>
+      <ListGroup className="mt-4 flex-row">
+        {cards.map(({ image }) => (
+          <img className="mr-4" style={{ width: "200px" }} src={image} />
+        ))}
+      </ListGroup>
+    </Container>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
