cc47270c1ea50d174c82d0af2665685ecd4dad69
Raphael Suter <raphael.werner.suter@gmail.com>
2021-01-12 11:12:13 +0100

    Aufgabe 3

--- src/index.js
+++ src/index.js
@@ -23,20 +23,55 @@ const App = () => {
       });
   };
 
+  const compare = ({ value: value1 }, { value: value2 }) => {
+    const values = [
+      "ACE",
+      "KING",
+      "QUEEN",
+      "JACK",
+      "10",
+      "9",
+      "8",
+      "7",
+      "6",
+      "5",
+      "4",
+      "3",
+      "2",
+    ];
+
+    const index1 = values.indexOf(value1);
+    const index2 = values.indexOf(value2);
+
+    if (index1 > index2) {
+      return 1;
+    } else {
+      return -1;
+    }
+  };
+
+  const filter = (suit) => {
+    return cards
+      .filter(({ suit: currentSuit }) => currentSuit === suit)
+      .sort(compare)
+      .map(({ image }) => (
+        <img className="mr-2" style={{ width: "100px" }} src={image} />
+      ));
+  };
+
   return (
-    <Container className="my-5">
+    <Container className="m-5">
       <div className="d-flex align-items-center">
         <Button onClick={newDeck}>New Deck</Button>
-        <p className="m-0 ml-3">{deckID}</p>
+        <p className="m-0 mx-3">{deckID}</p>
+        <Button onClick={newCard} disabled={!deckID}>
+          New Card
+        </Button>
       </div>
-      <Button className="mt-4" onClick={newCard}>
-        New Card
-      </Button>
-      <ListGroup className="mt-4 flex-row">
-        {cards.map(({ image }) => (
-          <img className="mr-4" style={{ width: "200px" }} src={image} />
-        ))}
-      </ListGroup>
+      <ListGroup className="mt-2 flex-row">{filter("HEARTS")}</ListGroup>
+      <ListGroup className="mt-2 flex-row">{filter("CLUBS")}</ListGroup>
+      <ListGroup className="mt-2 flex-row">{filter("SPADES")}</ListGroup>
+      <ListGroup className="mt-2 flex-row">{filter("DIAMONDS")}</ListGroup>
     </Container>
   );
 };
